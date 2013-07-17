dfcae4254d2383b92ce3eb62df80185e9da4e16b
Simon Mathieu <simon.math@gmail.com>
2013-07-17 11:56:12 -0700

    Update qualification score

---
+++ lib/rturk/operations/update_qualification_score.rb
@@ -0,0 +1,19 @@
+#http://docs.aws.amazon.com/AWSMechTurk/latest/AWSMturkAPI/ApiReference_UpdateQualificationScoreOperation.html
+module RTurk
+  class UpdateQualificationScore < Operation
+    require_params :qualification_type_id, :subject_id, :integer_value
+    attr_accessor :qualification_type_id, :subject_id, :integer_value
+    
+    def to_params
+      {'QualificationTypeId' => qualification_type_id,
+       'WorkerId' => subject_id,
+       'IntegerValue' => integer_value}
+    end
+  end
+  
+  def self.UpdateQualificationScore(*args)
+    RTurk::UpdateQualificationScore.create(*args)
+  end
+  
+end
+
---
+++ spec/operations/update_qualification_score_spec.rb
@@ -0,0 +1,30 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
+
+describe RTurk::UpdateQualificationScore do
+  before(:all) do
+    aws = YAML.load(File.open(File.join(SPEC_ROOT, 'mturk.yml')))
+    RTurk.setup(aws['AWSAccessKeyId'], aws['AWSAccessKey'], :sandbox => true)
+    faker('update_qualification_score', :operation => 'UpdateQualificationScore')
+  end
+  let(:valid_params) do
+    {
+      :qualification_type_id => '789RVWYBAZW00EXAMPLE', :subject_id => "XXX", :integer_value => 1
+    }
+  end
+  it "should ensure required params" do
+    lambda{RTurk::UpdateQualificationScore()}.should raise_error RTurk::MissingParameters
+  end
+
+  it "should successfully request the operation" do
+    RTurk::Requester.should_receive(:request).once.with(
+      hash_including('Operation' => 'UpdateQualificationScore'))
+    RTurk::UpdateQualificationScore(valid_params) rescue RTurk::InvalidRequest
+  end
+
+  it "should parse and return the result" do
+    response = RTurk::UpdateQualificationScore(valid_params)
+    response.elements.should == 
+      {"UpdateQualificationScoreResult"=>{"Request"=>{"IsValid"=>"True"}}}
+  end
+end
+
